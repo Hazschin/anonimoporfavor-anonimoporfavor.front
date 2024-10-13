a483a86e581ea85374085ef167abd8a4fa070fc3
McOrwell <setar-bosnai@hotmail.com>
2024-10-13 18:40:48 -0500

    Había un problema al extraer el string de la nota. Corregido

--- src/components/SaveButton/SaveButton.tsx
+++ src/components/SaveButton/SaveButton.tsx
@@ -10,16 +10,17 @@ export default function SaveButton() {
     const titleInput = document?.querySelector("#title") as HTMLInputElement;
     const authorInput = document?.querySelector("#author") as HTMLInputElement;
 
-    const note =
-      document?.querySelector("#editor")?.firstElementChild?.innerHTML;
+    const note = document?.querySelector("div#editorContainer div.ql-editor")
+      ?.firstElementChild?.innerHTML;
     const title = titleInput.value;
     const author = authorInput.value;
+    console.log({ note, title, author });
 
     fetchPostData<void>(`/notes/postNote`, { note, title, author })
       .then(() => {
         console.log("Tu nota se ha guardado correctamente");
         handleClose();
-        window.location.href = "/1";
+        //window.location.href = "/1";
       })
       .catch(() => {
         alert("Ha habido un problema al guardar tu nota");
